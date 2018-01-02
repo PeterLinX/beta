fdc2f793563c88a1764601ddad086d1a268a41fc
AaronBesson <aaronbesson@gmail.com>
2018-01-02 14:41:30 -0400

    clean

--- app/components/LoginLedgerNanoS.js
+++ app/components/LoginLedgerNanoS.js
@@ -36,7 +36,10 @@ export default class LoginLedgerNanoS extends Component<Props> {
         <div className="">Login using the Ledger Nano S:</div>
         <div className="">
           <div>
-            
+            <div className="login-button" onClick={this.onLedgerNanoSChange}>Use Ledger Nano S</div>
+          </div>
+          <p>{hardwareDeviceInfo}</p>
+          <p>{hardwarePublicKeyInfo}</p>
         </div>
       </div>
     )
