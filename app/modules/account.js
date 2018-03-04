9d78190ce0d21300590ea8558b68f2e58bdb443a
AaronBesson <aaronbesson@gmail.com>
2018-03-03 20:38:08 -0400

    BTC fix
    Fixed BTC account from previous ledger update

--- app/modules/account.js
+++ app/modules/account.js
@@ -3,6 +3,10 @@ import { ledgerNanoSCreateSignatureAsync } from "../modules/ledger/ledgerNanoS";
 
 // Constants
 export const LOGIN = "LOGIN";
+export const BTC_LOGIN = "BTCLOGIN";
+export const LTC_LOGIN = "LTCLOGIN";
+export const BTC_LOGIN_REDIRECT = "BTCLOGIN_REDIRECT";
+export const LTC_LOGIN_REDIRECT = "LTCLOGIN_REDIRECT";
 export const LOGOUT = "LOGOUT";
 export const SET_ADDRESS = "SET_ADDRESS";
 export const SET_DECRYPTING = "SET_DECRYPTING";
@@ -178,6 +182,36 @@ export function setPublicKey(publicKey) {
   };
 }
 
+export function ltcLogIn(pa, pk) {
+	return {
+		type: LTC_LOGIN,
+		pa: pa,
+		pk: pk
+	};
+}
+
+export function btcLogIn(pa, pk){
+	return {
+		type: BTC_LOGIN,
+		pa: pa,
+		pk: pk
+	};
+}
+
+export function ltcLoginRedirect(path) {
+	return {
+		type: LTC_LOGIN_REDIRECT,
+		path: path
+	};
+}
+
+export function btcLoginRedirect(path) {
+	return {
+		type: BTC_LOGIN_REDIRECT,
+		path: path,
+	};
+}
+
 // Reducer that manages account state (account now = private key)
 export default (
   state = {
@@ -187,7 +221,13 @@ export default (
     redirectUrl: null,
     decrypting: false,
     accountKeys: [],
-    publicKey: null
+    publicKey: null,
+    btcLoggedIn: false,
+		btcPrivKey: null,
+		btcPubAddr: null,
+		ltcLoggedIn: false,
+		ltcPrivKey: null,
+		ltcPubAddr: null
   },
   action
 ) => {
@@ -231,6 +271,30 @@ export default (
       return { ...state, decrypting: action.state };
     case SET_KEYS:
       return { ...state, accountKeys: action.keys };
+      case BTC_LOGIN:
+    		return {
+    			...state,
+    			btcLoggedIn: true,
+    			btcPrivKey: action.pk,
+    			btcPubAddr: action.pa
+    		};
+    	case LTC_LOGIN:
+    		return {
+    			...state,
+    			ltcLoggedIn: true,
+    			ltcPrivKey: action.pk,
+    			ltcPubAddr: action.pa
+    		};
+    	case BTC_LOGIN_REDIRECT:
+    		return {
+    			...state,
+    			btcLoginRedirect: action.path
+    		};
+    	case LTC_LOGIN_REDIRECT:
+    		return {
+    			...state,
+    			ltcLoginRedirect: action.path
+    		};
     default:
       return state;
   }
