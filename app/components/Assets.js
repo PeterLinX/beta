bdbbaf4c12a69441a6f23bfa2a3d79222fdb0028
AaronBesson <aaronbesson@gmail.com>
2018-03-07 12:37:36 -0400

    new nep template
    removed neo/gas code from NEP pages.

--- app/components/Assets.js
+++ app/components/Assets.js
@@ -8,15 +8,6 @@ import { setMarketPrice, resetPrice } from "../modules/wallet";
 import { sendEvent, clearTransactionEvent } from "../modules/transactions";
 import { initiateGetBalance, intervals } from "../components/NetworkSwitch";
 
-import bitcoinLogo from "../img/btc-logo.png";
-import ltcLogo from "../img/litecoin.png";
-import rpxLogo from "../img/rpx.png";
-import qlinkLogo from "../img/qlink.png";
-import thekeyLogo from "../img/thekey.png";
-import nexLogo from "../img/nex.png";
-import deepLogo from "../img/deep.png";
-import hashpuppiesLogo from "../img/hashpuppies.png";
-
 
 // force sync with balance data
 const refreshBalance = async (dispatch, net, address) => {
@@ -34,9 +25,12 @@ class Assets extends Component {
 		this.state = {
 			gasPrice: 0,
 			dbcPrice: 0,
+			ontPrice: 0,
 			qlcPrice: 0,
-			rhtPrice: 0,
-			rpxPrice: 0
+			rpxPrice: 0,
+			tkyPrice: 0,
+			tncPrice: 0,
+			zptPrice: 0
 		};
 	}
 
@@ -47,39 +41,17 @@ class Assets extends Component {
 
 				<div className="row top-10 dash-portfolio center">
 
-				<Link  to={ "/sendBTC" }>
+
+				<Link to="/sendDBC">
 					<div className="col-5">
-						<span className="market-price">BTC {numeral(this.props.marketBTCPrice).format("$0,0.00")}</span>
-						<h3>{numeral(this.props.btc).format("0,0.00000")} <span className="btc-price"> BTC</span></h3>
+						<span className="market-price">DBC {numeral(this.props.marketDBCPrice).format("$0,0.00")}</span>
+						<h3>{numeral(
+							Math.floor(this.props.dbc * 100000) / 100000
+						).format("0,0.0000")} <span className="dbc-price"> DBC</span></h3>
 						<hr className="dash-hr" />
-						<span className="market-price">{numeral(this.props.btc * this.props.marketBTCPrice).format("$0,0.00")} USD</span>
+						<span className="market-price">{numeral(this.props.dbc*this.props.marketDBCPrice).format("$0,0.00")} USD</span>
 					</div>
 				</Link>
-
-
-					<Link to="/sendRPX">
-						<div className="col-5">
-							<span className="market-price">RPX {numeral(this.props.marketRPXPrice).format("$0,0.00")}</span>
-							<h3>{numeral(
-								Math.floor(this.props.rpx * 100000) / 100000
-							).format("0,0.0000")} <span className="rpx-price"> RPX</span></h3>
-							<hr className="dash-hr" />
-							<span className="market-price">{numeral(this.props.rpx*this.props.marketRPXPrice).format("$0,0.00")} USD</span>
-						</div>
-					</Link>
-
-
-					<Link to="/sendDBC">
-						<div className="col-5">
-							<span className="market-price">DBC {numeral(this.props.marketDBCPrice).format("$0,0.00")}</span>
-							<h3>{numeral(
-								Math.floor(this.props.dbc * 100000) / 100000
-							).format("0,0.0000")} <span className="dbc-price"> DBC</span></h3>
-							<hr className="dash-hr" />
-							<span className="market-price">{numeral(this.props.dbc*this.props.marketDBCPrice).format("$0,0.00")} USD</span>
-						</div>
-					</Link>
-
 
 					<Link to="/sendQLC">
 						<div className="col-5">
@@ -92,16 +64,40 @@ class Assets extends Component {
 						</div>
 					</Link>
 
-
-					<Link to="/sendHP">
+					<Link to="/sendRPX">
 						<div className="col-5">
-							<span className="market-price">Priceless</span>
+							<span className="market-price">RPX {numeral(this.props.marketRPXPrice).format("$0,0.00")}</span>
 							<h3>{numeral(
-								Math.floor(this.props.rht * 1000000000)).format("0,0")} <span className="hp-price"> RHPT</span></h3>
+								Math.floor(this.props.rpx * 100000) / 100000
+							).format("0,0.0000")} <span className="rpx-price"> RPX</span></h3>
 							<hr className="dash-hr" />
-							<span className="market-price">$0.00 USD</span>
+							<span className="market-price">{numeral(this.props.rpx*this.props.marketRPXPrice).format("$0,0.00")} USD</span>
 						</div>
 					</Link>
+
+					<Link to="/sendTNC">
+						<div className="col-5">
+							<span className="market-price">TNC {numeral(this.props.marketTNCPrice).format("$0,0.00")}</span>
+							<h3>{numeral(
+								Math.floor(this.props.tnc * 100000) / 100000
+							).format("0,0.0000")} <span className="hp-price"> TNC</span></h3>
+							<hr className="dash-hr" />
+							<span className="market-price">{numeral(this.props.tnc*this.props.marketTNCPrice).format("$0,0.00")} USD</span>
+						</div>
+					</Link>
+
+					<Link to="/sendZPT">
+						<div className="col-5">
+							<span className="market-price">ZPT {numeral(this.props.marketZPTPrice).format("$0,0.00")}</span>
+							<h3>{numeral(
+								Math.floor(this.props.zpt * 100000) / 100000
+							).format("0,0.0000")} <span className="neo-price"> ZPT</span></h3>
+							<hr className="dash-hr" />
+							<span className="market-price">{numeral(this.props.zpt*this.props.marketZPTPrice).format("$0,0.00")} USD</span>
+						</div>
+					</Link>
+
+
 
 
 				</div>
@@ -117,7 +113,8 @@ const mapStateToProps = state => ({
 	dbc: state.wallet.Dbc,
 	qlc: state.wallet.Qlc,
 	rpx: state.wallet.Rpx,
-	rht: state.wallet.Rht,
+	tnc: state.wallet.Tnc,
+	zpt: state.wallet.Zpt,
 	address: state.account.address,
 	net: state.metadata.network,
 	price: state.wallet.price,
@@ -128,6 +125,7 @@ const mapStateToProps = state => ({
 	marketDBCPrice: state.wallet.marketDBCPrice,
 	marketQLCPrice: state.wallet.marketQLCPrice,
 	marketRPXPrice: state.wallet.marketRPXPrice,
+	marketTNCPrice: state.wallet.marketTNCPrice,
 	marketZPTPrice: state.wallet.marketZPTPrice,
 	btcLoggedIn: state.account.btcLoggedIn,
 	btcPrivKey: state.account.btcPrivKey,
